c311c9aa73c7966c44d325c088281dafa68c4d6e
bastien <bastien@wink-dev.com>
2020-03-04 12:40:07 +0100

    TP 3

---
+++ tp3/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<?php
+    echo '<h1>Hello world</h1>';
+    echo '<p>My name is Bastien</p>';
+?>
+
+</body>
+</html>
