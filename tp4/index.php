f54f41d814e51616f03276220be58c6fb926ab98
bastien <bastien@wink-dev.com>
2020-03-09 12:06:12 +0100

    TP 4

---
+++ tp4/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<?php
+
+//$a = 42;
+//var_dump($a);
+//$b = 13.37;
+//var_dump($b);
+//$c = 'coucou';
+//var_dump($c);
+//$d = true;
+//var_dump($d);
+//$c = null;
+//var_dump($c);
+//
+//$a2 = (float)'-3.1415926535897932384626433832795';
+//var_dump($a2);
+//$b2 = (int)'bastien';
+//var_dump($b2);
+//$c2 = '12';
+//var_dump($c2);
+
+//$note_maths = 15;
+//$note_francais = 12;
+//$note_histoire_geo = 9;
+//$moyenne = ($note_maths + $note_francais + $note_histoire_geo) / 3;
+//echo 'La moyenne est de '.$moyenne.' / 20.';
+
+//$prix_ht = 50;
+//$tva = 20;
+//$prix_ttc = $prix_ht * 1.2;
+//echo 'Le prix TTC du produit est de '.$prix_ttc.' €.';
+
+$test = 42;
+$test = (string)$test;
+var_dump($test);
+
+?>
+
+</body>
+</html>
