0180374c7697da3ae53f3a61cc0306cd6bb0f600
bastien <bastien@wink-dev.com>
2020-03-10 14:45:35 +0100

    TP 7

---
+++ tp12/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Hop</title>
+</head>
+<body>
+
+<h1>Mes film</h1>
+
+<table>
+    <tr>
+        <td>Titre</td>
+        <td>Note</td>
+    </tr>
+    <tr>
+        <td>Le parrain</td>
+        <td>9.2</td>
+    </tr>
+    <tr>
+        <td>Pulp Fiction</td>
+        <td>8.9</td>
+    </tr>
+    <tr>
+        <td>Fight Club</td>
+        <td>8.8</td>
+    </tr>
+</table>
+
+<hr/>
+
+<h3>Ajouter</h3>
+<form>
+    Nom du film : <input type="text"><br/>
+    Note : <input type="number"><br/>
+    <input type="submit">
+</form>
+
+</body>
+</html>
